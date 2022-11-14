d173ac5ef28b8c3444bec6b92534953c4f9b30e4
c0063vgw <c063vgw@yamaguchi-u.ac.jp>
2022-11-14 12:04:22 +0900

    app.blade修正

--- recipeApp/resources/views/layouts/app.blade.php
+++ recipeApp/resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
             <div class="container">
                 <a href="{{ url('/search') }}">
                     <!--img src="../../../database/img/recipeApp.png" alt="くっくらべる"-->
-                    <img src="{{ asset('img/recipeApp.png') }}" alt="くっくらべる" width=280">
+                    <img src="{{ asset('img/recipeApp.png') }}" alt="くっくらべる" width="280">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
