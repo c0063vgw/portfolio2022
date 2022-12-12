5619f439364dc8360161589b19b59b3cc220c443
c0063vgw <c063vgw@yamaguchi-u.ac.jp>
2022-12-12 12:13:02 +0900

    マージン再々修正

--- recipeApp/resources/views/similar.blade.php
+++ recipeApp/resources/views/similar.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header d-flex bg-grad-grade py-2">
                     <div class="text-white lead mt-1">似ているレシピ一覧</div>
-                    <div class="lead mr-3" style="margin-left: auto"><span class="text-dark">難易度</span>
+                    <div class="lead mr-3" style="margin-left: auto"><span class="text-dark mt-1">難易度</span>
                         @if( $request->simiLevel == "asc")
                         <a class="btn btn-danger btn_sm ml-1" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">
                             <i class="fas fa-sort-amount-down mr-1"></i>High</a>
